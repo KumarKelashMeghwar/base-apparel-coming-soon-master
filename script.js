954df78a81d21dbf637478e86ec1bfac9d59a7aa
Kelash <58991510+KumarKelashMeghwar@users.noreply.github.com>
2020-12-31 20:14:49 +0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const image = document.querySelector(".image");
-
-if(window.innerWidth <= 470){    
-    image.src = "hero-mobile.jpg";
-    image.style.backgroundSize = "contain";
-    image.style.width = "400px";
-    image.style.height = "250px";
-}
-
